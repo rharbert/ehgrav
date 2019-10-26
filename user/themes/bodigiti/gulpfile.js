aee95c5a9d6738ab794be51524aac093c2aa5608
Rachael <rachael@bodigiti.com>
2019-10-26 13:36:40 -0400

    Theme JavaScript Minify: Removes all blank/white space.
    run with: gulp js-mini
    
    1) Created two subfolders in theme/js
    	A. theme/js/source: contains js files for editing
    	B. theme/js/dist: contains js files from source, but have been
    	minified
    
    2) Updated javascript file references in the twig templates for all js
    files to now look for them in theme/js/source (instead of original
    theme/js)
    	ORIGINAL FILE NAMES AND LOCATIONS, NOW IN: js/source and js/dist
    	js/isotope.pkgd.js
    	js/imagesloaded.pkgd.js
    	js/lazysizes.min.js
    	js/lightslider-custom-zoom.js
    	js/lightslider.js
    	js/site.js
    	js/snipcart-custom.js
    
    3) Enabled gulpfile.js task 'js-mini' which when ran, takes all js files
    in 'js/source', minifies them (removes blank/white space) and saves them to
    the 'js/dist' directory.

--- user/themes/bodigiti/gulpfile.js
+++ user/themes/bodigiti/gulpfile.js
@@ -1,3 +1,39 @@
+/* Gulp Tasks for Rachael
+
+!!! SASS/CSS working method (refer to below commands)
+    1) run 'gulp sass' as you are working on the 'dev' site and not yet pushing
+    the changes to production
+
+    2) once you are happy with your sass/css
+      a. run: gulp sass-prod
+      b. run: gulp minify-css
+
+    gulp sass
+    ---> converts .scss files to .css and includes the source maps so you
+    can see in the browser 'inspector' which file and line the css styles
+    are coming from
+
+    gulp sass-prod
+    ---> does same as above, except excludes the source maps, saving lots
+    of file size
+
+    gulp minify-css
+    ---> removes all blank/white space from .css files, saving files size
+
+!!! JAVASCRIPT working method (refer to below commands)
+    1) edit the files in theme/js/source
+    2) run: gulp js-mini
+    3) refresh browser
+
+    gulp js-mini
+    ---> removes all blank/white space from .js files, saving file size.  
+    It takes all the files from theme/js/source/ and 
+    saves them to theme/js/dist/ ---> this directory is where the theme
+    looks for your js files... that is, all theme js files in twig have been
+    updated to find the files in theme/js/dist/
+
+*/
+
 // Require Gulp!
 // http://stackoverflow.com/questions/32490328/gulp-autoprefixer-throwing-referenceerror-promise-is-not-defined
 require('es6-promise').polyfill();
@@ -53,33 +89,33 @@ gulp.task('sass-prod', function() {
 // add 'ignore' css selectors for those that are added to page by JS or the css
 // selectors will be deleted even though they are needed
 //
-gulp.task('uncss', function() {
-  return gulp.src([
-      'style/css/styles.css'
-    ])
-    .pipe(uncss({
-      ignore: [
-        '.snipcart-wrapper.cart-not-empty',
-        '#snipcart-close .js-icon-text',
-        '.toggle-button__off',
-        '.toggle-button__on',
-        '.toggle-button-icon .handle',
-        '.toggle-button-icon.on',
-        '.toggle-button-icon.on .handle',
-        '.toggle-button-icon.on.red',
-        '.toggle-button-icon.on.red .handle'
-      ],
-      html: [
-        'https://jaxgrav.test/',
-        'https://jaxgrav.test/offline.html'
-      ]
-    }))
-    // Prefixes here after uncss because uncss delete prefixes since the prefixes
-    // do not appear in chrome browser 'localhost:8000' where uncss is looking for
-    // css selectors to keep
-    .pipe(prefix('last 2 versions', '>1%', 'ie 8')) 
-    .pipe(gulp.dest('style/css'));
-});
+// gulp.task('uncss', function() {
+//   return gulp.src([
+//       'style/css/styles.css'
+//     ])
+//     .pipe(uncss({
+//       ignore: [
+//         '.snipcart-wrapper.cart-not-empty',
+//         '#snipcart-close .js-icon-text',
+//         '.toggle-button__off',
+//         '.toggle-button__on',
+//         '.toggle-button-icon .handle',
+//         '.toggle-button-icon.on',
+//         '.toggle-button-icon.on .handle',
+//         '.toggle-button-icon.on.red',
+//         '.toggle-button-icon.on.red .handle'
+//       ],
+//       html: [
+//         'https://jaxgrav.test/',
+//         'https://jaxgrav.test/offline.html'
+//       ]
+//     }))
+//     // Prefixes here after uncss because uncss delete prefixes since the prefixes
+//     // do not appear in chrome browser 'localhost:8000' where uncss is looking for
+//     // css selectors to keep
+//     .pipe(prefix('last 2 versions', '>1%', 'ie 8')) 
+//     .pipe(gulp.dest('style/css'));
+// });
 
 // Minify CSS
 gulp.task('minify-css', function() {
@@ -93,13 +129,13 @@ gulp.task('minify-css', function() {
 //
 // Minify JS
 // Gulp task to minify JavaScript files
-// gulp.task('js-mini', function() {
-//   return gulp.src('js/source/*.js')
-//     // Minify the file
-//     .pipe(uglifyJS())
-//     // Output
-//     .pipe(gulp.dest('js/dist'))
-// });
+gulp.task('js-mini', function() {
+  return gulp.src('js/source/*.js')
+    // Minify the file
+    .pipe(uglifyJS())
+    // Output
+    .pipe(gulp.dest('js/dist'))
+});
 // // Concatenate JS Files
 // gulp.task('js-concat', function() {
 //   return gulp.src(['js/dist/site.js', 'js/dist/snipcart-custom.js', 'js/dist/service-worker-initiate.js',])
@@ -125,27 +161,27 @@ gulp.task('clean', function(cb) {
 });
 
 // Default
-gulp.task('default', ['sass', 'watch']);
+// gulp.task('default', ['sass', 'watch']);
 
 // Run Tasks sequentially with 'runSequence' module so mifiy runs after uncss and not before
-gulp.task('dist', function(done) {
-  runSequence('sass', 'sass-prod', 'uncss', 'minify-css', 'scripts', function() {
-      done();
-  });
-});
+// gulp.task('dist', function(done) {
+//   runSequence('sass', 'sass-prod', 'uncss', 'minify-css', 'scripts', function() {
+//       done();
+//   });
+// });
 
 // Sprite
-gulp.task('sprite', function() {
-  var spriteData =
-    gulp.src('images/sprite-these/*.*') // source path of the sprite images
-      .pipe(spritesmith({
-          imgName: 'sprite-manufacturer-logos.png',
-          cssName: '_sprite-manufacturer-logos.scss',
-      }));
+// gulp.task('sprite', function() {
+//   var spriteData =
+//     gulp.src('images/sprite-these/*.*') // source path of the sprite images
+//       .pipe(spritesmith({
+//           imgName: 'sprite-manufacturer-logos.png',
+//           cssName: '_sprite-manufacturer-logos.scss',
+//       }));
 
-  spriteData.img.pipe(gulp.dest('images/')); // output path for the sprite
-  spriteData.css.pipe(gulp.dest('style/scss/init/sprites/')); // output path for the SCSS
-});
+//   spriteData.img.pipe(gulp.dest('images/')); // output path for the sprite
+//   spriteData.css.pipe(gulp.dest('style/scss/init/sprites/')); // output path for the SCSS
+// });
 
 // Handle the error
 function errorHandler (error) {
